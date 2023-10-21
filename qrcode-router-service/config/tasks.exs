5320d9a5baf1a5a0b6228cf7d957fd4520e9273a
Renan Medina <renan@silvamedina.com.br>
2023-10-21 01:56:20 -0300

    Fixing configuration for scheduling cronjobs using quantum correctly and moving files

--- qrcode-router-service/config/tasks.exs
+++ qrcode-router-service/config/tasks.exs
@@ -3,5 +3,5 @@ import Config
 config :qrcode_router_service, QrcodeRouterService.TaskScheduler,
   jobs: [
     # Every Hour
-    {"0 0 * * *", fn -> Music.Importing.import_new_played_songs() end }
+    {"0 0 * * *", {Music.Importing, :import_new_played_songs, []}}
   ]
